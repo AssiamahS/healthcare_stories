8b2c50647e0b73d86da9710220905d43cec1fa1a
SylvesterAssiamah <122726691+AssiamahS@users.noreply.github.com>
2024-12-03 13:41:23 -0500

    Delete stories/healthcare_stories/urls.py

--- stories/healthcare_stories/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from stories import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('submit/', views.submit_story, name='submit_story'),
-]
